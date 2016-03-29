d4e425e712473cfc065bf3641e84ed2954c878c2
marcos-utn <elcorreodemarcos@gmail.com>
2016-03-29 19:22:05 -0300

    modifico la carpeta donde estaba la app de clase

--- MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/MainActivity.java
+++ MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b = (Button)findViewById(R.id.button);
-        b.setText("Hola desde java");
+        b.setText("Hola desde java 2 ");
     }
 }
