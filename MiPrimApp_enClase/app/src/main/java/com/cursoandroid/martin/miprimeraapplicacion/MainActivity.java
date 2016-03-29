059f476ea88c2492f83c4db340a4f8b1b2608cb3
marcos-utn <elcorreodemarcos@gmail.com>
2016-03-29 20:54:19 -0300

    le paso objetos a la clase que escucha

--- MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/MainActivity.java
+++ MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/MainActivity.java
@@ -3,6 +3,7 @@ package com.cursoandroid.martin.miprimeraapplicacion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button b = (Button)findViewById(R.id.button);
         b.setText("Hola desde java 2 ");
-        OrejaEscuchaBoton oreja = new OrejaEscuchaBoton();
+        TextView t = (TextView)findViewById(R.id.textView);
+        OrejaEscuchaBoton oreja = new OrejaEscuchaBoton(t);
         b.setOnClickListener(oreja);
     }
 }
--- MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/OrejaEscuchaBoton.java
+++ MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/OrejaEscuchaBoton.java
@@ -2,13 +2,19 @@ package com.cursoandroid.martin.miprimeraapplicacion;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by android on 29/03/16.
  */
 public class OrejaEscuchaBoton implements View.OnClickListener{
+    TextView t;
+    public OrejaEscuchaBoton(TextView txt){
+        t = txt;
+    }
     @Override
     public void onClick(View v) {
         Log.d("tagoreja.onclick","marcha un click");
+        t.setText("ando!");
     }
 }
