9c9b4a19a5cf948778eadf77eaf462cadccb516b
marcos-utn <elcorreodemarcos@gmail.com>
2016-03-29 21:11:18 -0300

    de otra forma...

--- MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/MainActivity.java
+++ MiPrimApp_enClase/app/src/main/java/com/cursoandroid/martin/miprimeraapplicacion/MainActivity.java
@@ -2,19 +2,24 @@ package com.cursoandroid.martin.miprimeraapplicacion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button b = (Button)findViewById(R.id.button);
-        b.setText("Hola desde java 2 ");
+        b.setText("Hola desde java 3 ");
+        // OrejaEscuchaBoton oreja = new OrejaEscuchaBoton(t);
+        b.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
         TextView t = (TextView)findViewById(R.id.textView);
-        OrejaEscuchaBoton oreja = new OrejaEscuchaBoton(t);
-        b.setOnClickListener(oreja);
+        t.setText("ando! tambien");
     }
 }
