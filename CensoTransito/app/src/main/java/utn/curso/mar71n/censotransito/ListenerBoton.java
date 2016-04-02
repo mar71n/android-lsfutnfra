ce069888a64344ae177bca1ce4a6710bcf3e927b
mar71n <mar71n@gmail.com>
2016-04-02 10:25:19 -0300

    creo una clase que implemente View.OnClickListener para escuchar el evento click del boton

---
+++ CensoTransito/app/src/main/java/utn/curso/mar71n/censotransito/ListenerBoton.java
@@ -0,0 +1,14 @@
+package utn.curso.mar71n.censotransito;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/**
+ * Created by Usuario on 2/4/2016.
+ */
+public class ListenerBoton implements OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
