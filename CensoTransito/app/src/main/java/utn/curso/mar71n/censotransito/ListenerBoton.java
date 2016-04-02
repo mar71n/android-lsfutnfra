ac5e83b9ba95983a028680d7cc907229769215bb
mar71n <mar71n@gmail.com>
2016-04-02 11:12:46 -0300

    al listener le inyecto la dependencia del texto, lo seteo en boton, y totalizo de prepo

--- CensoTransito/app/src/main/java/utn/curso/mar71n/censotransito/ListenerBoton.java
+++ CensoTransito/app/src/main/java/utn/curso/mar71n/censotransito/ListenerBoton.java
@@ -2,13 +2,23 @@ package utn.curso.mar71n.censotransito;
 
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+
 
 /**
  * Created by Usuario on 2/4/2016.
  */
 public class ListenerBoton implements OnClickListener {
+    private TextView tt;
+
+    public ListenerBoton(TextView txttot) {
+        this.tt = txttot;
+    }
+
     @Override
     public void onClick(View v) {
-
+        Integer actual = Integer.parseInt( tt.getText().toString());
+        actual++;
+        tt.setText(actual.toString());
     }
 }
--- CensoTransito/app/src/main/java/utn/curso/mar71n/censotransito/MainCTActivity.java
+++ CensoTransito/app/src/main/java/utn/curso/mar71n/censotransito/MainCTActivity.java
@@ -2,6 +2,8 @@ package utn.curso.mar71n.censotransito;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainCTActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainCTActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ct);
+        Button bcontar = (Button) findViewById(R.id.button);
+        TextView txtTotal = (TextView) findViewById(R.id.txtTot);
+        ListenerBoton lb1 = new ListenerBoton(txtTotal);
+        bcontar.setOnClickListener(lb1);
     }
 }
