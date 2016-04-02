ec1ec6cb563394e760aeb8512fdae7cd9e65e8aa
mar71n <mar71n@gmail.com>
2016-04-02 10:08:55 -0300

    agrego el proyecto CensoTransito

---
+++ CensoTransito/app/src/main/java/utn/curso/mar71n/censotransito/MainCTActivity.java
@@ -0,0 +1,13 @@
+package utn.curso.mar71n.censotransito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ct);
+    }
+}
