79e9335b462d224a321cf64eb8ffed33dbf6ef10
mar71n <mar71n@gmail.com>
2016-04-10 14:05:41 -0300

    un listenr para los botones

---
+++ EjCalculadora/app/src/main/java/utn/curso/mar71n/ejcalculadora/EscucharBotones.java
@@ -0,0 +1,13 @@
+package utn.curso.mar71n.ejcalculadora;
+
+import android.view.View;
+
+/**
+ * Created by Usuario on 10/4/2016.
+ */
+public class EscucharBotones implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
