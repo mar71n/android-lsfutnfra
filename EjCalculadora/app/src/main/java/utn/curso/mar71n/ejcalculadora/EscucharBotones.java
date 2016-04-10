66f88c3cf56400617b49c1e172ebd7689402c092
mar71n <mar71n@gmail.com>
2016-04-10 14:27:16 -0300

    todos con el mismo listener inyecto las dependencias

--- EjCalculadora/app/src/main/java/utn/curso/mar71n/ejcalculadora/EscucharBotones.java
+++ EjCalculadora/app/src/main/java/utn/curso/mar71n/ejcalculadora/EscucharBotones.java
@@ -1,11 +1,23 @@
 package utn.curso.mar71n.ejcalculadora;
 
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Usuario on 10/4/2016.
  */
 public class EscucharBotones implements View.OnClickListener {
+    TextView txtdisplay;
+    TextView txtcalculado;
+    int[] lidbotones;
+    Calculadorita calcu;
+    public EscucharBotones(TextView display, TextView calculadora, int[] lidBotones) {
+        txtdisplay  = display;
+        txtcalculado = calculadora;
+        lidbotones = lidBotones;
+        calcu = new Calculadorita();
+    }
+
     @Override
     public void onClick(View v) {
 
--- EjCalculadora/app/src/main/java/utn/curso/mar71n/ejcalculadora/MainECActivity.java
+++ EjCalculadora/app/src/main/java/utn/curso/mar71n/ejcalculadora/MainECActivity.java
@@ -2,6 +2,8 @@ package utn.curso.mar71n.ejcalculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainECActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainECActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ec);
+        int[] lidBotones = new int[]{R.id.bt0, R.id.bt1, R.id.bt2, R.id.bt3, R.id.bt4, R.id.bt5,
+                R.id.bt6, R.id.bt7, R.id.bt8, R.id.bt9, R.id.btad, R.id.btsus,
+                R.id.btmul, R.id.btdiv, R.id.btigual};
+        TextView display = (TextView)findViewById(R.id.textViewDisplay);
+        TextView calculadora = (TextView)findViewById(R.id.textViewCalcu);
+        EscucharBotones listener = new EscucharBotones(display, calculadora, lidBotones);
+        for( int i = 0 ; i <lidBotones.length; i++){
+            Button b = (Button)findViewById(lidBotones[i]);
+            b.setOnClickListener(listener);
+        }
     }
 }
