ca004b6732584058fc2977caa1e2bc80184a81f4
mar71n <mar71n@gmail.com>
2016-04-12 14:19:11 -0300

    para pruebas del lenguaje

---
+++ javas/PruevaArray.java
@@ -0,0 +1,26 @@
+class PruebaArray {
+    public static void main(String[] args) {
+       // Declaración del array
+        int[] arrayDeEnteros;
+       
+       // Creamos 5 espacios para enteros
+       arrayDeEnteros = new int[5];
+
+       arrayDeEnteros[0] = 1;
+       arrayDeEnteros[1] = 3;
+       arrayDeEnteros[2] = 5;
+       arrayDeEnteros[3] = 7;
+       arrayDeEnteros[4] = 9;
+
+       System.out.println("arrayDeEnteros[0]: "
+                  + arrayDeEnteros[0]);
+       System.out.println("arrayDeEnteros[1]: "
+                  + arrayDeEnteros[1]);
+       System.out.println("arrayDeEnteros[0]: "
+                  + arrayDeEnteros[2]);
+      System.out.println("arrayDeEnteros[0]: "
+                 + arrayDeEnteros[3]);
+      System.out.println("arrayDeEnteros[0]: "
+                 + arrayDeEnteros[4]);
+  }
+}
