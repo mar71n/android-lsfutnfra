70562fa83a18c19e0d26aac2fa9a0026374d1edd
mar71n <mar71n@gmail.com>
2016-04-12 20:34:17 -0300

    listas

--- Clase4_layout/app/src/main/java/com/cursoandroid/martin/clase4_layout/MainLY4Activity.java
+++ Clase4_layout/app/src/main/java/com/cursoandroid/martin/clase4_layout/MainLY4Activity.java
@@ -1,7 +1,12 @@
 package com.cursoandroid.martin.clase4_layout;
 
+import android.content.pm.LabeledIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainLY4Activity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainLY4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.otrolayout);
+
+        List<String> lista = new ArrayList<>();
+        Log.d("activity", "tamannio:" + lista.size());
+        lista.add("opcion 1");
+        lista.add("opcion 2");
+        lista.add("opcion 3");
+        Log.d("activity", "tamannio:" + lista.size());
+        lista.remove(1);
+        Log.d("activity", "tamannio:" + lista.size());
+
+        for (String a:lista) {
+            Log.d("activity",a);
+        }
+
     }
 }
