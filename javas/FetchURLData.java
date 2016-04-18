57d34a21b7013ce6c077622763e6529ff42cc3d8
mar71n <mar71n@gmail.com>
2016-04-18 15:31:57 -0300

    conectar a internet

---
+++ javas/FetchURLData.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+ 
+ /**
+  * @author Crunchify.com
+   */
+    
+public class FetchURLData {
+     
+     	public static void main(String[] args) {
+		try {
+                      URL url = new URL("http://www.metrovias.com.ar//Subterraneos//Estado?site=Metrovias");
+		      BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+		      String strTemp = "";
+		while (null != (strTemp = br.readLine())) {
+         		System.out.println(strTemp);
+	        	}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+ 	}
+}
