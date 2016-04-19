71d7187f6832b002943b1e272074df3067ac032d
mar71n <mar71n@gmail.com>
2016-04-19 20:22:41 -0300

    un ViewHolder para el adapter

--- Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MyAdapter.java
+++ Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MyAdapter.java
@@ -1,6 +1,8 @@
 package com.cursoandroid.martin.lista_en_clase5;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
@@ -17,7 +19,11 @@ public class MyAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.item_layout, parent, false);
+        RecyclerView.ViewHolder vh = new MyViewHolder(v);
+        return vh;
     }
 
     @Override
@@ -27,6 +33,6 @@ public class MyAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lista.size();
     }
 }
---
+++ Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MyViewHolder.java
@@ -0,0 +1,13 @@
+package com.cursoandroid.martin.lista_en_clase5;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by android on 19/04/16.
+ */
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(View itemView) {
+        super(itemView);
+    }
+}
