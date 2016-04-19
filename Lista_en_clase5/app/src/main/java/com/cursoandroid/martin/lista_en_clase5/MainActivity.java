5072ff0fdcd286f4e22485e30a1a64debc83fca2
mar71n <mar71n@gmail.com>
2016-04-19 20:44:23 -0300

    mas en el actyvity y en el adapter

--- Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MainActivity.java
+++ Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MainActivity.java
@@ -2,6 +2,7 @@ package com.cursoandroid.martin.lista_en_clase5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -14,13 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    List<Persona> lista = new ArrayList<Persona>();
-    Persona p1 = new Persona("Juan", "Perez");
-    Persona p2 = new Persona("Luis", "Gomez");
-    Persona p3 = new Persona("Luis", "Gomez");
+        List<Persona> lista = new ArrayList<Persona>();
+        Persona p1 = new Persona("Juan", "Perez");
+        Persona p2 = new Persona("Luis", "Gomez");
+        Persona p3 = new Persona("Luis", "Gomez");
 
-    lista.add(p1);
-    lista.add(p2);
-    lista.add(p3);
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(p3);
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.list);
+
+        MyAdapter adapter = new MyAdapter(lista);
+
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager l = new LinearLayoutManager(this);
+        rv.setLayoutManager(l);
     }
 }
--- Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MyAdapter.java
+++ Lista_en_clase5/app/src/main/java/com/cursoandroid/martin/lista_en_clase5/MyAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -28,7 +29,12 @@ public class MyAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        Persona p = lista.get(position);
+        View v = holder.itemView;
+        TextView tNombre = (TextView) v.findViewById(R.id.txtNombre);
+        TextView tApellido = (TextView) v.findViewById(R.id.txtApellido);
+        tNombre.setText(p.getNombre());
+        tApellido.setText(p.getApellido());
     }
 
     @Override
