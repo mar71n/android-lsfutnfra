15d07b4dae1358eb90547dc00e4de63ed04b2373
mar71n <mar71n@gmail.com>
2016-05-02 15:10:20 -0300

    Ejercicio completar intents, inicial

---
+++ EjercicioParaCompletarIntents/app/src/main/java/com/lslutnfra/ejercicioparacompletarintents/MainActivity.java
@@ -0,0 +1,28 @@
+package com.lslutnfra.ejercicioparacompletarintents;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+
+
+    }
+}
