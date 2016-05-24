31b277d882b4597ba4285b548f6bf150e3503d03
mar71n <mar71n@gmail.com>
2016-05-24 20:34:13 -0300

    toolbar share y search

--- Menunlase9/app/src/main/java/com/example/android/menunlase9/MainActivity.java
+++ Menunlase9/app/src/main/java/com/example/android/menunlase9/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.android.menunlase9;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener, SearchView.OnQueryTextListener {
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,14 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"http://www.lslutnfra.com");
+
+        mShareActionProvider.setShareIntent(intent);
         return true;
     }
 
@@ -88,5 +102,17 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
                 break;
             }
         }
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.d(getLocalClassName(),"Busca : " + query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.d(getLocalClassName(),"Busca : " + newText);
+        return false;
     }
 }
