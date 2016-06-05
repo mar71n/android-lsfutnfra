078f30aa525d7343840f79834d117c24a92ead29
mar71n <mar71n@gmail.com>
2016-06-05 14:42:43 -0300

    agrego los dos layout que faltaban y los tres fragment

---
+++ TPIJuegoMemoria/app/src/main/java/utn/curso/mar71n/tpijuegomemoria/AyudaFragment.java
@@ -0,0 +1,30 @@
+package utn.curso.mar71n.tpijuegomemoria;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import utn.curso.mar71n.tpijuegomemoria.R;
+
+/**
+ * Created by android on 31/05/16.
+ */
+public class AyudaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle
+            savedInstanceState) {
+
+        // Creamos la View para este fragment y la devolvemos
+        ViewGroup rootView = (ViewGroup)
+                inflater.inflate(R.layout.layout_ayuda,
+                        container, false);
+
+        // Cargamos la View con contenido, seteamos listeners, etc.
+        //...
+
+        return rootView;
+    }
+}
