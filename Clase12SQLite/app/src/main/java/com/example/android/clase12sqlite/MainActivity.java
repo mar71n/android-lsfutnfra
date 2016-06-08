7d689d74399307d4bfccd58edf84fe30f7cbe39a
mar71n <mar71n@gmail.com>
2016-06-07 21:26:12 -0300

    una consulta sql

--- Clase12SQLite/app/src/main/java/com/example/android/clase12sqlite/MainActivity.java
+++ Clase12SQLite/app/src/main/java/com/example/android/clase12sqlite/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.clase12sqlite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyOpenHelper helper = new MyOpenHelper(this,"Personas.db3");
+
+        SQLiteDatabase db =  helper.getWritableDatabase();
+
+        Cursor c;
+
+        c = db.rawQuery("SELECT _id,nombre,edad FROM Personas WHERE _id=1",null);
+
+        if(c.moveToFirst())
+        {
+
+            long id = c.getLong(0);
+            String nombre = c.getString(1);
+            int edad = c.getInt(2);
+            Log.d("db", "Persona. id:" + id + " nombre:" + nombre + " edad:" + edad);
+        }
+        c.close();
+        db.close();
+        helper.close();
+
     }
 }
