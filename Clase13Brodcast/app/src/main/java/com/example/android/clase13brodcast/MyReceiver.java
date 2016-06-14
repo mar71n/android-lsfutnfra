0dd53c5f9155e1ac42f0a89d4e5a9089b2ecb0b3
mar71n <mar71n@gmail.com>
2016-06-14 20:58:53 -0300

    notificaciones

--- Clase13Brodcast/app/src/main/java/com/example/android/clase13brodcast/MyReceiver.java
+++ Clase13Brodcast/app/src/main/java/com/example/android/clase13brodcast/MyReceiver.java
@@ -1,9 +1,13 @@
 package com.example.android.clase13brodcast;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
@@ -27,6 +31,31 @@ public class MyReceiver extends BroadcastReceiver {
                 Log.d("br","Texto:"+messages[i].getMessageBody());
             }
         }
+        NotificationManager mNotificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        int icon = R.mipmap.ic_launcher;
+        String tickerText = "Evento de sms";
+        long when = System.currentTimeMillis();
+
+        CharSequence contentTitle = "SMS";
+        CharSequence contentText = "Evento de sms";
+
+        Intent notificationIntent = new Intent(context,MainActivity.class);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder  builder = new NotificationCompat.Builder(context);
+
+        Notification notification = builder.setContentIntent(contentIntent)
+                .setSmallIcon(icon)
+                .setTicker(tickerText)
+                .setWhen(when)
+                .setAutoCancel(true)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .addAction(R.mipmap.ic_launcher, "Boton",contentIntent)
+                .build();
+        mNotificationManager.notify(0, notification);
     }
 
 }
