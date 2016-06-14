5281aaab0b98d5301267ca7dddf140510a539ffe
mar71n <mar71n@gmail.com>
2016-06-14 19:59:24 -0300

    un receiver que captura sms

--- Clase13Brodcast/app/src/main/java/com/example/android/clase13brodcast/MyReceiver.java
+++ Clase13Brodcast/app/src/main/java/com/example/android/clase13brodcast/MyReceiver.java
@@ -3,6 +3,9 @@ package com.example.android.clase13brodcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class MyReceiver extends BroadcastReceiver {
     public MyReceiver() {
@@ -12,6 +15,18 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        Bundle info = intent.getExtras();
+        if(info!=null){
+            Object[] pduArray = (Object[]) info.get("pdus");
+            SmsMessage[] messages = new SmsMessage[pduArray.length];
+            for(int i=0;i<pduArray.length;i++)
+            {
+                messages[i]=SmsMessage.createFromPdu((byte[])pduArray[i]);
+                Log.d("br", "Msg de:" + messages[i].getOriginatingAddress());
+                Log.d("br","Texto:"+messages[i].getMessageBody());
+            }
+        }
     }
+
 }
