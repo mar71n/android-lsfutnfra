c1034ba8800fe140994a6fe1ca0fdc226cdae2a1
mar71n <mar71n@gmail.com>
2016-06-14 21:23:29 -0300

    camara

--- Clase13Brodcast/app/src/main/java/com/example/android/clase13brodcast/MainActivity.java
+++ Clase13Brodcast/app/src/main/java/com/example/android/clase13brodcast/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.android.clase13brodcast;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(this);
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == Activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            // obtenemos un bitmap con una preview de la imagen tomada
+            ImageView imageViewPreView=(ImageView)findViewById(R.id.imageView);
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // mostramos el bitmap
+            imageViewPreView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, 0);
+
+    }
 }
