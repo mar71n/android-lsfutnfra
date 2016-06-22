c8ffff56b44d67e19e2ea2818434d5b72e32b313
mar71n <mar71n@gmail.com>
2016-06-21 21:16:36 -0300

    mapas

--- Clase14GPS/app/src/main/java/com/example/android/clase14gps/MainActivity.java
+++ Clase14GPS/app/src/main/java/com/example/android/clase14gps/MainActivity.java
@@ -3,10 +3,21 @@ package com.example.android.clase14gps;
 import android.content.Context;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     //  AIzaSyAvxZLeeQ9MbTU6XzzjDGrKe4F4SqrIk_E
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +34,38 @@ public class MainActivity extends AppCompatActivity {
 
         }catch(SecurityException e){}
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) fragmentManager.findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng posObelisco = new LatLng(-34.603559, -58.381478);
+        MarkerOptions markerObelisco = new MarkerOptions();
+        markerObelisco.position(posObelisco);
+        markerObelisco.title("Obelisco");
+        markerObelisco.snippet("67,5m");
+        markerObelisco.icon(BitmapDescriptorFactory.
+                fromResource(R.mipmap.ic_launcher));
+        Marker m = googleMap.addMarker(markerObelisco);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posObelisco, 15));
+
+        // Creamos una polylinea
+        PolylineOptions pol = new PolylineOptions();
+        pol.add(posObelisco);
+        pol.add(new LatLng(-34.607284, -58.381390));
+        pol.add(new LatLng(-34.607398, -58.383483));
+        pol.add(new LatLng(-34.609169, -58.383507));
+        pol.color(0x80FF0000); // Rojo con transparencia
+        pol.width(6); // ancho (0 a 10)
+        googleMap.addPolyline(pol);
+
+        //googleMap.setMyLocationEnabled(true);
     }
 }
