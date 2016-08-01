35b30b6f12732bbab3dccfc5b59e53c1203261da
mar71n <mar71n@gmail.com>
2016-08-01 13:16:00 -0300

    pide url si no se conecta

--- javas/FetchURLData.java
+++ javas/FetchURLData.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
 * @author Crunchify.com
@@ -8,18 +9,41 @@ import java.net.URL;
 
 public class FetchURLData {
     public static void main(String[] args) {
+        leerUrl();
+    }
+
+    private static void leerUrl(){
         try {
-            System.setProperty("http.proxyHost", "proxy.dgeyc");
-            System.setProperty("http.proxyPort", "3128");
             URL url = new URL("http://www.metrovias.com.ar//Subterraneos//Estado?site=Metrovias");
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             String strTemp = "";
             while (null != (strTemp = br.readLine())) {
                 System.out.println(strTemp);
             }
-        } catch (Exception ex) {
+        }catch (java.net.UnknownHostException ex){
+            System.err.println("No se puede encontrar : " + ex.getMessage());
+            ingresarProxy();
+        }catch (Exception ex) {
             ex.printStackTrace();
             System.err.println(ex.toString());
+            System.err.println(ex.getMessage());
         }
+    }
+
+    private static void ingresarProxy(){
+        System.out.println ("Por favor introduzca http.proxyHost:");
+
+        String proxyHost = "";
+        Scanner entradaEscaner = new Scanner (System.in); //Creación de un objeto Scanner
+        proxyHost = entradaEscaner.nextLine (); //Invocamos un método sobre un objeto Scanner
+        System.out.println ("Entrada recibida por teclado es: \"" + proxyHost +"\"");
+        System.out.println ("Por favor introduzca http.proxyPort:");
+
+        String proxyPort = "";
+        proxyPort = entradaEscaner.nextLine (); //Invocamos un método sobre un objeto Scanner
+        System.out.println ("Entrada recibida por teclado es: \"" + proxyPort +"\"");
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+        leerUrl();
     }
 }
